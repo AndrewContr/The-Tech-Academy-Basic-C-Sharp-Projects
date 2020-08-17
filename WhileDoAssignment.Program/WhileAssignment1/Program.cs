3cc15209bb013aee074ac33d3fd7dc4f3393bf34
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-08-16 23:51:40 -0600

    Add files via upload

---
+++ WhileDoAssignment.Program/WhileAssignment1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            bool correct = guess == 49;
+
+            while (!correct)
+            {
+                switch (guess)
+                {
+                    case 12:
+                        Console.WriteLine("12 is mistaken. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 48:
+                        Console.WriteLine("You guessed 48. Close, but try again.");
+                        Console.WriteLine("Guess a number?");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 50:
+                        Console.WriteLine("You guessed 50. Close, but try again.");
+                        Console.WriteLine("Guess a number?");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 49:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        correct = true;
+                        break;
+                    default:
+                        Console.WriteLine("Nope.");
+                        Console.WriteLine("Guess a number?");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+                   Console.ReadLine();
+        }
+    }
+}
+        
+    
+
