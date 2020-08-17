fef63e85d91c31c4f0532c0c0ddb80af34f5d961
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-08-17 01:00:42 -0600

    Add files via upload

---
+++ StringAssignment.Program/StringAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string con = "Welcome to ";
+            string cate = "Jurassic Park: ";
+            string nate = "Dubai, UAE.";
+
+            cate = cate.ToUpper();
+            
+            Console.WriteLine(con + cate + nate);
+
+            StringBuilder shinning = new StringBuilder();
+
+            shinning.Append("All work and no play\n");
+            shinning.Append("makes Jack a dull boy.\n");
+            shinning.Append("However, no TV and no beer\n");
+            shinning.Append("makes Homer go crazy.");
+
+            Console.WriteLine(shinning);
+
+            Console.ReadLine();
+        }
+    }
+}
