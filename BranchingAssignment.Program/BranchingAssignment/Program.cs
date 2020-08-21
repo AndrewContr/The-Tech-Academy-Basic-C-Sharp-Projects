6bdfef44174868211e59a956901c57513c77ef4e
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-08-21 14:39:40 -0600

    Add files via upload

--- BranchingAssignment.Program/BranchingAssignment/Program.cs
+++ BranchingAssignment.Program/BranchingAssignment/Program.cs
@@ -11,34 +11,36 @@ namespace BranchingAssignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
-            
+
             Console.WriteLine("Please enter the weight of the package.");
             int packWeight = Convert.ToInt32(Console.ReadLine());
             if (packWeight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
             }
             else
             {
                 Console.WriteLine("Please enter the width of the package.");
-            }
-            int packWidth = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter the height of the package.");
-            int packHeight = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter the length of the package.");
-            int packLength = Convert.ToInt32(Console.ReadLine());
-            if (packWidth + packHeight + packLength > 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
-                Console.ReadLine();
-            }
-            else
-            {
-                decimal outCome = ((packWidth * packHeight * packLength) * packWeight) / 100;
-                Console.WriteLine("Your estimated total for shipping this package is: " + "$" + outCome);
-                Console.WriteLine("Thank you.");
-                
-                Console.ReadLine();
+
+                int packWidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the height of the package.");
+                int packHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the length of the package.");
+                int packLength = Convert.ToInt32(Console.ReadLine());
+                if (packWidth + packHeight + packLength > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    decimal outCome = ((packWidth * packHeight * packLength) * packWeight) / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: " + "$" + outCome);
+                    Console.WriteLine("Thank you.");
+
+                    Console.ReadLine();
+                }
             }
         }
     }
