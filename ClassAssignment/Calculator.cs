b35801b185010da333e0c89c688793313bcb0421
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-06 02:56:21 -0600

    Add files via upload

---
+++ ClassAssignment/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Calculator
+    {
+        public static int addOne(int number)
+        {
+            return number + 1;
+        }
+        public static int squareNum(int number)
+        {
+            return number * number;
+        }
+        public static int minusOne(int number)
+        {
+            return number - 1;
+        }
+    }
+}
---
+++ ClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int testSub = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Calculator.addOne(testSub));
+            Console.WriteLine(Calculator.squareNum(testSub));
+            Console.WriteLine(Calculator.minusOne(testSub));
+            Console.ReadLine();
+            ;
+            
+        }
+    }
+}
