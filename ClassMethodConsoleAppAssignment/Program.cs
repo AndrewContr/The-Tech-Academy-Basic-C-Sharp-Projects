b6fab3e08d5f582496eeb7fc139169dd399f281b
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-07 05:23:17 -0600

    Add files via upload

---
+++ ClassMethodConsoleAppAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer.");
+            int wholeNum = Convert.ToInt32(Console.ReadLine());
+            Tabulator userInt = new Tabulator();
+            Console.WriteLine(userInt.mathOp(wholeNum));
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter a decimal number.");
+            decimal decNum = Convert.ToDecimal(Console.ReadLine());
+            Tabulator userDec = new Tabulator();
+            Console.WriteLine(userDec.mathOp(decNum));
+            Console.ReadLine();
+
+            Console.WriteLine("Please write a number as a string.");
+            short fromString = (short)Convert.ToInt32(Console.ReadLine());
+            Tabulator userString = new Tabulator();
+            Console.WriteLine(userString.mathOp(fromString));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassMethodConsoleAppAssignment/Tabulator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodConsoleAppAssignment
+{
+    class Tabulator
+    {
+        public int mathOp(int number)
+        {
+            return number + 1;
+        }
+    
+        public int mathOp(decimal number)
+        {
+            return (int)number * 4; 
+        }
+
+        public int mathOp(short number)
+        {
+            return number -1;       
+        } 
+    }
+}
