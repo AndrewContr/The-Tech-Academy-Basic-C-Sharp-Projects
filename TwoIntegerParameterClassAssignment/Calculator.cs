a7eac5e53f63316a5fc71746638684727f78bcd9
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-10 00:33:51 -0600

    Add files via upload

---
+++ TwoIntegerParameterClassAssignment/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntegerParameterClassAssignment
+{
+    public class Calculator
+    {
+       public int addTwo(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+        public int addTwo(int number1)
+        {
+            return number1 + 5;
+        }
+    }
+}
