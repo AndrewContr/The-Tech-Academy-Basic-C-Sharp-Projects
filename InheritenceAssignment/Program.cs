8ffcc47afd1f42d9cef4b61ccb1fb7b397d6f87f
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-11 02:57:40 -0600

    Add files via upload

---
+++ InheritenceAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritenceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee worker = new Employee();
+            worker.FirstName = "Sample";
+            worker.LastName = "Student";
+            worker.SayName();
+            Console.ReadLine();
+        }
+    }
+}
