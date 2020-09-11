c545a4f5e462a14abaa0d7f2abf478ea2f89d021
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-11 01:22:25 -0600

    Add files via upload

---
+++ VoidMethodAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabulator uLa = new Tabulator();
+            uLa.stuff(number1: 3, number2: 4);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VoidMethodAssignment/Tabulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Tabulator
+    {
+        public void stuff( int number1, int number2)
+        {
+            Console.WriteLine(number1 + 5);
+            Console.WriteLine(number2);
+        }
+    }
+}
