1e502d8d081644d8cd98c44c2220132f2b4e8fe6
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-13 03:58:23 -0600

    Add files via upload

---
+++ InterfacesAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAssignment
+{
+    public class Employee : IQuittable
+    {
+       public void Quit(int number)
+        {
+            Console.WriteLine(number + 1);
+        }
+    }
+}
---
+++ InterfacesAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAssignment
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable quitter = new Employee();
+            quitter.Quit(5);
+            Console.ReadLine();
+        }
+    }
+}
