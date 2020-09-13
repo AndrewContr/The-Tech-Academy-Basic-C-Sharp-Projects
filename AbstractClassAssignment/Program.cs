e436b2fbcfd78249bbc95bf7b2b7eda684994208
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-13 03:10:02 -0600

    Add files via upload

---
+++ AbstractClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee worker = new Employee();
+            worker.firstName = "Sample";
+            worker.lastName = "Student";
+            worker.SayName();
+            Console.ReadLine();
+        }
+    }
+}
