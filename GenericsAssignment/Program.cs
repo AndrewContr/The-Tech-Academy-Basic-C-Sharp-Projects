5b39dd0b4e1038175106bb2bdb8cc006e71d3a40
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-14 03:03:20 -0600

    Add files via upload

---
+++ GenericsAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> doozy = new Employee<string>();
+            doozy.Things = new List<string>() { "Chubby", "Babs", "Kurt", "Rockefeller", "Mandible" };
+
+            Employee<int> crumbsly = new Employee<int>();
+            crumbsly.Things = new List<int>() { 48, 9, 69, 70, 234 };
+
+            foreach (int thing in crumbsly.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            
+            foreach (string thang in doozy.Things)
+            {
+                Console.WriteLine(thang);
+            }
+            Console.ReadLine();
+        }
+    }
+}
