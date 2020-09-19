10cd2cc542eb49054612d1700e0e74b2b760593e
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-19 00:51:43 -0600

    Add files via upload

---
+++ TextLogAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextLogAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int numNum = Convert.ToInt32(Console.ReadLine());
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\andre\log.txt", true))
+            {
+                file.WriteLine(numNum);
+            }
+
+        }
+    }
+}
