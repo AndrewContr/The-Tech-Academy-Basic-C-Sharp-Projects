1b0b57a502bcdc5407ff8b347caef09f663a13e4
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-19 01:47:57 -0600

    Add files via upload

---
+++ DateTimeAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number.");
+            int timeNum = Convert.ToInt32(Console.ReadLine());
+            DateTime future = DateTime.Now + TimeSpan.FromHours(timeNum); 
+            Console.WriteLine(future);
+            Console.ReadLine();
+        }
+    }
+}
