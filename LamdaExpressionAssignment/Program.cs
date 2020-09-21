fd3f35d196839698569453c36d822dc206264aaa
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-21 02:36:59 -0600

    Add files via upload

--- LamdaExpressionAssignment/Program.cs
+++ LamdaExpressionAssignment/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,18 +14,19 @@ namespace LamdaExpressionAssignment
         {
             List<Employee> averageJoes = new List<Employee>();
 
-            List<Employee> Employees = new List<Employee>();
+            List<Employee> Employees = new List<Employee>
             {
-                new Employee { firstName = "Gord", lastName = "Buell", ID = 1 };
-                new Employee { firstName = "Shep", lastName = "Sheppard", ID = 2 };
-                new Employee { firstName = "Butch", lastName = "Weaver", ID = 3 };
-                new Employee { firstName = "Joe", lastName = "Knutsen", ID = 4 };
-                new Employee { firstName = "Range", lastName = "Blllhlll", ID = 5 };
-                new Employee { firstName = "Villanova", lastName = "Giraffe", ID = 6 };
-                new Employee { firstName = "Joe", lastName = "Hoisin", ID = 7 };
-                new Employee { firstName = "Planters", lastName = "Monopoly", ID = 8 };
-                new Employee { firstName = "Ewel", lastName = "Peck", ID = 9 };
-                new Employee { firstName = "Anderson", lastName = "Cooper", ID = 10 };
+                new Employee { firstName = "Gord", lastName = "Buell", ID = 1 },
+                new Employee { firstName = "Shep", lastName = "Sheppard", ID = 2 },
+                new Employee { firstName = "Butch", lastName = "Weaver", ID = 3 },
+                new Employee { firstName = "Joe", lastName = "Knutsen", ID = 4 },
+                new Employee { firstName = "Range", lastName = "Blllhlll", ID = 5 },
+                new Employee { firstName = "Villanova", lastName = "Giraffe", ID = 6 },
+                new Employee { firstName = "Joe", lastName = "Hoisin", ID = 7 },
+                new Employee { firstName = "Planters", lastName = "Monopoly", ID = 8 },
+                new Employee { firstName = "Ewel", lastName = "Peck", ID = 9 },
+                new Employee { firstName = "Anderson", lastName = "Cooper", ID = 10 },
+            };
 
     
                 foreach (Employee employee in Employees)
@@ -34,17 +36,29 @@ namespace LamdaExpressionAssignment
                         averageJoes.Add(employee);
                     }
                 }
-                Console.WriteLine(averageJoes);
-                Console.ReadLine();
-
-
-                List<Employee> joeList = Employees.Where(x => x.firstName == "Joe").ToList();
-
-
-                List<Employee> fiveList = Employees.Where(x => x.ID > 5).ToList();
-
+            foreach (Employee emp in averageJoes)
+            {
+                Console.WriteLine(emp.firstName + emp.lastName);
             }
+            Console.ReadLine();
+
+
+            List<Employee> joeList = Employees.Where(x => x.firstName == ("Joe")).ToList();
+            
+            foreach (Employee x in joeList)
+            {
+                Console.WriteLine(x.firstName + x.lastName);
+            }
+
+            List<Employee> fiveList = Employees.Where(x => x.ID > 5).ToList();
+
+            foreach (Employee y in fiveList)
+            {
+                Console.WriteLine(y.ID + y.firstName + y.lastName);    
+            }
+            Console.ReadLine(); 
+        
         }
     }
-
 }
+
