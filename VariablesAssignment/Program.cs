2c84516dbf2b88b51859f0cca8019677a0b46da0
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-24 01:30:13 -0600

    Add files via upload

---
+++ VariablesAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string immute = "Korean Cinema";
+            var options = new Ssalc("Gerbil", "O'Conchshell");
+        }
+    }
+}
---
+++ VariablesAssignment/Ssalc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAssignment
+{
+    class Ssalc
+    {
+        public Ssalc(string firstname) : this(firstname, string.Empty)
+        {
+        }
+
+        public Ssalc(string firstname, string lastname)
+        {
+            this.firstName = firstname;
+            this.lastName = lastname;
+            
+        }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    }
+    
+}
