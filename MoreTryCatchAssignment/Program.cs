e5c52c0588af98123e0060877498fac3f35162fa
AndrewContr <61569744+AndrewContr@users.noreply.github.com>
2020-09-25 05:28:37 -0600

    Add files via upload

---
+++ MoreTryCatchAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreTryCatchAssignment
+{
+    public class Program
+    {
+       static void Main(string[] args)
+        {
+           
+                Console.WriteLine("Please enter your age");
+                try
+                {
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    if (age <= 0)
+                    {
+                        Console.WriteLine("Please enter only numbers greater than zero.");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        TimeSpan lifeTime = DateTime.Now.AddYears(age) - DateTime.Now;
+                        DateTime birthYear = DateTime.Now - lifeTime;
+                        Console.WriteLine(birthYear.Year);
+                        Console.ReadLine();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("An error occurred. Please contact your system administrator.");
+                    Console.ReadLine();
+                    return;
+                }
+                
+              
+            }
+        }
+    }
